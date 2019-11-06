0416bae1c9b25605b83576e61875bb2b836452e4
PetarUe <56553349+PetarUe@users.noreply.github.com>
2019-11-06 18:03:21 +0100

    add file

---
+++ exercise_java/dzava.js
@@ -0,0 +1,75 @@
+
+
+/*
+function setColor() {
+    var boja = document.querySelectorAll('ul')[1];
+    boja.className = 'red';
+}
+setColor();
+
+function setClr() {
+    var set = document.getElementsByTagName('li');
+    for (var i = 0; i < set.length; i++) {
+        set[i].className = "green";
+    }
+
+}
+setClr();
+
+function setLast() {
+    var last = document.querySelectorAll('ul')[2];
+    var lista = last.querySelectorAll('li');
+
+
+    for (var i = 0; i < lista.length; i++) {
+        if (i % 2 === 0) {
+            lista[i].className = "blue";
+        } else {
+            lista[i].className = "grey";
+        }
+    }
+}
+setLast();
+
+
+function setOne() {
+    var one = document.querySelector('.active');
+
+    one.className = '';
+
+    var two = document.querySelectorAll('div')[1];
+    var smt = two.firstElementChild.firstElementChild;
+    smt.className = 'active';
+
+
+}
+setOne();
+*/
+
+
+
+
+/* Access/Update Text Node
+Create an unordered list containing <li> items that represent navigation.
+
+Create a function that takes text from one of the <li> elements and presents it on screen using alert.
+
+Create one more function.
+The function should take some text as an argument.
+The function should select the last <li> element in the list and replace its text with text passed as the function argument.
+
+ */
+
+function takeText() {
+    var element = document.querySelector('section');
+    var element01 = element.firstElementChild.firstElementChild;
+
+
+
+
+
+
+    takeText();
+
+
+
